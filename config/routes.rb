438ac03a5324c8fe7276f8a6e8447d67b4b144e9
Ken Funakoshi <funaks@live.com>
2021-08-08 00:35:04 +0900

    added recognition controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get '/dashboard', to:'dashboard#show'
+  get '/dashboard', to:'dashboard#show', as: 'Dashboard'
+  get '/user/:id', to: 'user#show', as: 'User' 
+  get '/recognition/new', to: 'recognition#new'
+
 end
